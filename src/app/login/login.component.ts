18f6de1e259a885487bcd31d600d988a8311bebd
gabriel-remon <remongabi16@gmail.com>
2024-04-23 00:55:24 -0300

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,53 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { Component, EventEmitter, NgModule, Output, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { LocalStorageService } from '../service/local-storage.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatIconModule,NgClass,CommonModule],
+  imports: [MatIconModule,NgClass,CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  expression=false
+  formLogin= new FormGroup ({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
 
-  activateRegister(){
-    this.expression=false;
+  formRegister= new FormGroup ({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+  
+  localStorage = inject(LocalStorageService)
+
+  submitLogin(){
+   
+    console.log(
+      this.localStorage.login(this.formLogin.value)
+    )
+
   }
-  activateLogin(){
-    this.expression=true;
+  submitRegister(){
+    console.log(
+      this.localStorage.register( this.formRegister.value)?
+      "registro exitoso":"no se puedo registrar")
+  }
+
+  expression=false
+  mode="password";
+
+  changeMode(){
+    this.mode = this.mode==="password"?"text":"password";
+  }
+
+  changeLogin(){
+    this.expression= !this.expression;
   }
 
  cerrarComponenteYVolver() {
